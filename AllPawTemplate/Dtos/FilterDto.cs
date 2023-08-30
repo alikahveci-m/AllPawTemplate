65f404c1a081c6a7b1f58e94ec0c3fa7d4bb3ee3
turkogluemret@gmail.com <turkogluemret@gmail.com>
2023-08-31 00:28:28 +0300

    first commit

--- AllPawTemplate/Dtos/FilterDto.cs
+++ AllPawTemplate/Dtos/FilterDto.cs
@@ -3,7 +3,7 @@
     public class FilterDto
     {
         public List<string> Breeds { get; set; } // Seçilen türlerin listesi
-        public PriceRange PriceRange { get; set; } // Fiyat aralığı
+        public PriceRange PriceRange { get; set; } // Fiyat aralığ
         public List<string> Ages { get; set; } // Seçilen yaş aralıklarının listesi
         public string SellerType { get; set; } // Satıcı türü (Sahibinden veya Petshop)
     }
