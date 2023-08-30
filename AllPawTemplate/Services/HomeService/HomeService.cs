33633f2ab838de0f1662d9fa1bda15f66e5539d1
turkogluemret@gmail.com <turkogluemret@gmail.com>
2023-08-31 02:21:05 +0300

    hp logo changed

--- AllPawTemplate/Services/HomeService/HomeService.cs
+++ AllPawTemplate/Services/HomeService/HomeService.cs
@@ -1,9 +1,18 @@
 ﻿using AllPawTemplate.Models;
+using AllPawTemplate.Repositories.AdvertRepository;
+using AllPawTemplate.Repositories.CityRepository;
 
 namespace AllPawTemplate.Services.HomeService
 {
     public class HomeService : IHomeService
     {
+        private readonly IAdvertRepository _advertRepository;
+        private readonly ICityRepository _cityRepository;
+        public HomeService(IAdvertRepository advertRepository, ICityRepository cityRepository)
+        {
+            _advertRepository = advertRepository;
+            _cityRepository = cityRepository;
+        }
         public Task<HomeResponse> GetHome()
         {
             throw new NotImplementedException();
